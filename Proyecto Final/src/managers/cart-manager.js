065bdf1cd32236efbf2d020811c8238177218bc3
Florencia Palacio <137527381+FlorenciaPalacio@users.noreply.github.com>
2024-11-12 21:53:15 -0300

    Update cart-manager.js

--- Proyecto Final/src/managers/cart-manager.js
+++ Proyecto Final/src/managers/cart-manager.js
@@ -6,8 +6,7 @@ class CartManager {
         this.path = path;
         this.carts = [];
         this.ultId = 0;
-
-        //Cargar los carritos almacenados en el archivo: 
+ 
         this.cargarCarritos();
     }
 
@@ -16,13 +15,10 @@ class CartManager {
             const data = await fs.promises.readFile(this.path, "utf-8");
             this.carts = JSON.parse(data);
             if (this.carts.length > 0) {
-                //Verifico si hay por lo menos algun elemento y voy a calcular el ultimo id: 
-                this.ultId = Math.max(...this.carts.map(cart => cart.id));
-                //Utilizo el método map para crear un nuevo array que solo tenga los ids y con Math.Max obtengo el mayor, guardandolo en la propiedad ultId. 
+                this.ultId = Math.max(...this.carts.map(cart => cart.id)); 
             }
         } catch (error) {
-            console.log("Error al cargar el carrito");
-            //Si no existe el archivo, lo voy a crear: 
+            console.log("Error al cargar el carrito"); 
             await this.guardarCarritos();
         }
     }
@@ -30,19 +26,14 @@ class CartManager {
     async guardarCarritos() {
         await fs.promises.writeFile(this.path, JSON.stringify(this.carts, null, 2));
     }
-
-    //Metodo para crear un carrito: 
-
+    
     async crearCarrito() {
         const nuevoCarrito = {
             id: ++this.ultId,
             products: []
         };
 
-        //Este objeto "carrito" lo pusheamos al array: 
         this.carts.push(nuevoCarrito);
-
-        //Guardamos el array en el archivo: 
         await this.guardarCarritos();
         return nuevoCarrito;
     }
@@ -58,23 +49,20 @@ class CartManager {
             return carritoBuscado;
 
         } catch (error) {
-            console.log("Error al obtener el carrito por id, vamos a morir");
+            console.log("Error al obtener el carrito por id");
             throw error;
         }
     }
 
     async agregarProductoAlCarrito(carritoId, productoId, quantity = 1) {
         const carrito = await this.getCarritoById(carritoId);
-        const existeProducto = carrito.products.find(p => p.product === productoId);
-        //De esta forma chequeo si el producto que estoy recibiendo para agregar al carrito ya esta presente en el. Si existe modifico la cantidad, si no existe lo agrego. 
+        const existeProducto = carrito.products.find(p => p.product === productoId); 
 
         if (existeProducto) {
             existeProducto.quantity += quantity;
         } else {
             carrito.products.push({ product: productoId, quantity });
         }
-
-        //Como aca modifique el carrito, ahora tengo que guardar en el archivo: 
         await this.guardarCarritos();
         return carrito;
     }
