69720918c970f8680de3cba41150caf518e90b9b
Florencia Palacio <137527381+FlorenciaPalacio@users.noreply.github.com>
2024-11-12 22:02:23 -0300

    Update views.router.js

--- Proyecto Final/src/routes/views.router.js
+++ Proyecto Final/src/routes/views.router.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
 const router = Router(); 
-
-//Importamos al ProductModel: 
 import ProductModel from "../models/product.model.js";
 
 router.get("/products", async (req, res) => {
